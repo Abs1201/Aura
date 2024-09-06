024f9bbe8662bbc1595026241407cbada5a97dd2
KimSungHun <kimtiger1201@naver.com>
2024-09-06 15:56:26 +0900

    6. Player and Enemy Character

---
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -0,0 +1,5 @@
+// Copyright JackK
+
+
+#include "Character/AuraCharacter.h"
+
---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,17 @@
+// Copyright JackK
+
+
+#include "Character/AuraCharacterBase.h"
+
+// Sets default values
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
