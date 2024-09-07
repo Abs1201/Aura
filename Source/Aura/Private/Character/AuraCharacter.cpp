ef8725b4c4b962ea32112b25de1ac97a71b9d8f8
KimSungHun <kimtiger1201@naver.com>
2024-09-07 16:25:57 +0900

    12. Game Mode

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -2,4 +2,38 @@
 
 
 #include "Character/AuraCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
+
+AAuraCharacter::AAuraCharacter()
+{
+    CameraArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+    CameraArm->SetupAttachment(GetCapsuleComponent());
+    CameraArm->TargetArmLength = 750.f;
+    CameraArm->SetRelativeRotation(FRotator(-45.f, 0.f, 0.f));
+    CameraArm->bEnableCameraLag = true;
+    CameraArm->bInheritYaw = false;
+
+    FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
+    FollowCamera->SetupAttachment(CameraArm, USpringArmComponent::SocketName);
+
+    
+
+}
+
+void AAuraCharacter::BeginPlay()
+{
+    Super::BeginPlay();
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->RotationRate = FRotator(0.f, 400.f, 0.f);
+    GetCharacterMovement()->bConstrainToPlane = true;
+    GetCharacterMovement()->bSnapToPlaneAtStart = true;
+
+    bUseControllerRotationPitch = false;
+    bUseControllerRotationRoll = false;
+    bUseControllerRotationYaw = false;
+
+}
--- Source/Aura/Public/Character/AuraCharacter.h
+++ Source/Aura/Public/Character/AuraCharacter.h
@@ -6,6 +6,11 @@
 #include "Character/AuraCharacterBase.h"
 #include "AuraCharacter.generated.h"
 
+
+
+class USpringArmComponent;
+class UCameraComponent;
+
 /**
  * 
  */
@@ -13,5 +18,19 @@ UCLASS()
 class AURA_API AAuraCharacter : public AAuraCharacterBase
 {
 	GENERATED_BODY()
+
+public:
+	AAuraCharacter();
+
+protected:
+	virtual void BeginPlay() override;
+
+private:
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class USpringArmComponent* CameraArm;
+
+	UPROPERTY(VisibleAnywhere, Category = "Camera")
+	class UCameraComponent* FollowCamera;
+
 	
 };
