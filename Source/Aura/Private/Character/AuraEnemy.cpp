81ba6d2cffc781a376c48d7bd5e14afb578c32fd
KimSungHun <kimtiger1201@naver.com>
2024-09-07 17:18:24 +0900

    14. Highlight Enemies

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,11 +2,16 @@
 
 
 #include "Character/AuraEnemy.h"
+#include "DrawDebugHelpers.h"
+
 
 void AAuraEnemy::HighlightActor()
 {
+	//DrawDebugSphere(GetWorld(), GetActorLocation(), 20.f, 20, FColor::Blue, false, 5.f);
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
+	//DrawDebugSphere(GetWorld(), GetActorLocation(), 20.f, 20, FColor::Red, false, 5.f);
+
 }
