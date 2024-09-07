7877df82193c5540a0ffe347c209dd9ae6b9adec
KimSungHun <kimtiger1201@naver.com>
2024-09-08 00:57:19 +0900

    15. Post Process Highlight

--- Source/Aura/Private/Character/AuraEnemy.cpp
+++ Source/Aura/Private/Character/AuraEnemy.cpp
@@ -2,16 +2,41 @@
 
 
 #include "Character/AuraEnemy.h"
-#include "DrawDebugHelpers.h"
+#include "Aura/Aura.h"
 
+//#include "DrawDebugHelpers.h"
+
+
+AAuraEnemy::AAuraEnemy()
+{
+	GetMesh()->SetCollisionResponseToChannel(ECollisionChannel::ECC_Visibility, ECollisionResponse::ECR_Block);
+}
 
 void AAuraEnemy::HighlightActor()
 {
 	//DrawDebugSphere(GetWorld(), GetActorLocation(), 20.f, 20, FColor::Blue, false, 5.f);
+	GetMesh()->SetRenderCustomDepth(true);
+	Weapon->SetRenderCustomDepth(true);
+
+
 }
 
 void AAuraEnemy::UnHighlightActor()
 {
 	//DrawDebugSphere(GetWorld(), GetActorLocation(), 20.f, 20, FColor::Red, false, 5.f);
+	GetMesh()->SetRenderCustomDepth(false);
+	Weapon->SetRenderCustomDepth(false);
+
+
+}
+
+void AAuraEnemy::BeginPlay()
+{
+	Super::BeginPlay();
+
+	GetMesh()->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+	Weapon->SetCustomDepthStencilValue(CUSTOM_DEPTH_RED);
+
+
 
 }
