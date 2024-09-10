51798c4b758f507eec8059f4e1ca871cd99e1b32
KimSungHun <kimtiger1201@naver.com>
2024-09-10 15:07:10 +0900

    29. Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,10 @@
+// Copyright JackK
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright JackK
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
