4672e70ec14c0321e8518e097ea41f4f77aabf8f
KimSungHun <kimtiger1201@naver.com>
2024-09-19 18:52:13 +0900

    89. Attribute Info Data Asset

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -8,9 +8,9 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 {
 	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
 
-	const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
-	//GameplayTags.
-	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString()));
+	//FAuraGameplayTags::Get().Attributes_Primary_Intelligence
+	/*const FAuraGameplayTags& GameplayTags = FAuraGameplayTags::Get();
+	GEngine->AddOnScreenDebugMessage(-1, 10.f, FColor::Orange, FString::Printf(TEXT("Tag: %s"), *GameplayTags.Attributes_Secondary_Armor.ToString()));*/
 }
 
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent, const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
