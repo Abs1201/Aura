2e010126ca24fdafc95a42bf6f54313928d25cac
KimSungHun <kimtiger1201@naver.com>
2024-09-30 17:57:17 +0900

    129. Default Attribute Effect

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,8 @@
+// Copyright JackK
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
