7c2369aeaa72bcd1c2c0c5eba94e4934a3a0cb02
KimSungHun <kimtiger1201@naver.com>
2025-07-15 18:59:00 +0900

    267. Upgrading Attributes

--- Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/AttributeMenuWidgetController.cpp
@@ -5,6 +5,7 @@
 #include "AbilitySystem/AuraAttributeSet.h"
 #include "AbilitySystem/Data/AttributeInfo.h"
 #include "Player/AuraPlayerState.h"
+#include <AbilitySystem/AuraAbilitySystemComponent.h>
 
 
 
@@ -52,6 +53,12 @@ void UAttributeMenuWidgetController::BroadcastInitialValues()
 	AttributePointsChangedDelegate.Broadcast(AuraPlayerState->GetAttributePoints());
 
 
+}
+
+void UAttributeMenuWidgetController::UpgradeAttribute(const FGameplayTag& AttributeTag)
+{
+	UAuraAbilitySystemComponent* AuraASC = CastChecked<UAuraAbilitySystemComponent>(AbilitySystemComponent);
+	AuraASC->UpgradeAttribute(AttributeTag);
 }
 
 void UAttributeMenuWidgetController::BroadcastAttributeInfo(const FGameplayTag& AttributeTag, const FGameplayAttribute& Attribute) const
