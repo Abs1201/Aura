da514a422326800e55836d8a4ee431c8b17eafbd
KimSungHun <kimtiger1201@naver.com>
2025-10-18 18:40:41 +0900

    328. First Target Trace

--- Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -20,6 +20,8 @@ public:
 
 	UFUNCTION(BlueprintCallable)
 	void StoreOwnerVariables();
+	UFUNCTION(BlueprintCallable)
+	void TraceFirstTarget(const FVector& BeamTargetLocation);
 protected:
 
 	UPROPERTY(BlueprintReadWrite, Category = "Beam")
