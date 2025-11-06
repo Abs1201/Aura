1391d19e9927c0804490969bf08b64edf1a136cc
KimSungHun <kimtiger1201@naver.com>
2025-11-06 15:14:44 +0900

    349. Wait Input Press

--- Source/Aura/Private/Character/AuraCharacter.cpp
+++ Source/Aura/Private/Character/AuraCharacter.cpp
@@ -195,6 +195,7 @@ void AAuraCharacter::ShowMagicCircle_Implementation(UMaterialInterface* DecalMat
     if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
     {
         AuraPlayerController->ShowMagicCircle(DecalMaterial);
+        AuraPlayerController->bShowMouseCursor = false;
     }
 }
 
@@ -203,6 +204,7 @@ void AAuraCharacter::HideMagicCircle_Implementation()
     if (AAuraPlayerController* AuraPlayerController = Cast<AAuraPlayerController>(GetController()))
     {
         AuraPlayerController->HideMagicCircle();
+        AuraPlayerController->bShowMouseCursor = true;
     }
 }
 
